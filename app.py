a3db23a283209bb7c0d592ec76c8e7904f1827ea
andy <andytwoods@gmail.com>
2020-08-01 20:17:23 +0100

    yolo tooooo slow! 3+ minutes

--- app.py
+++ app.py
@@ -10,9 +10,55 @@ from flask import render_template
 import threading
 from imutils.video import VideoStream
 import time
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import tensorflow as tf
 import cv2
 import numpy as np
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
 
+IM_WIDTH = 640
+IM_HEIGHT = 480
+
+
+MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
+MODEL_LOCATION = 'object_detection'
+CWD_PATH = os.getcwd()
+PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_LOCATION, MODEL_NAME,'frozen_inference_graph.pb')
+PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_LOCATION, 'data','mscoco_label_map.pbtxt')
+NUM_CLASSES = 90
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+# Load the Tensorflow model into memory.
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+    sess = tf.Session(graph=detection_graph)
+
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+# Output tensors are the detection boxes, scores, and classes
+# Each box represents a part of the image where a particular object was detected
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+# Each score represents level of confidence for each of the objects.
+# The score is shown on the result image, together with the class label.
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+# Number of objects detected
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+camera_type = 'picamera'
 
 app = Flask(__name__)
 
@@ -26,7 +72,7 @@ print(f" * ngrok tunnel {public_url} -> http://127.0.0.1:{port}")
 
 # Update any base URLs or webhooks to use the public ngrok URL
 app.config["BASE_URL"] = public_url
-vs = VideoStream(usePiCamera=True).start()
+#vs = VideoStream(usePiCamera=True, resolution=(IM_WIDTH, IM_HEIGHT)).start()
 time.sleep(1.0)
 
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
@@ -81,18 +127,90 @@ def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     # return the resized image
     return resized
 
+@app.route("/car2")
+def car2():
+    frame_rate_calc = 1
+    freq = cv2.getTickFrequency()
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    camera = PiCamera()
+    camera.resolution = (IM_WIDTH, IM_HEIGHT)
+    camera.framerate = 10
+    rawCapture = PiRGBArray(camera, size=(IM_WIDTH, IM_HEIGHT))
+    rawCapture.truncate(0)
+    frame = None
+
+    for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+
+        t1 = cv2.getTickCount()
+
+        # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
+        # i.e. a single-column array, where each item in the column has the pixel RGB value
+        frame = np.copy(frame1.array)
+        frame.setflags(write=1)
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_expanded = np.expand_dims(frame_rgb, axis=0)
+
+        # Perform the actual detection by running the model with the image as input
+        (boxes, scores, classes, num) = sess.run(
+            [detection_boxes, detection_scores, detection_classes, num_detections],
+            feed_dict={image_tensor: frame_expanded})
+
+        # Draw the results of the detection (aka 'visulaize the results')
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            frame,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=8,
+            min_score_thresh=0.40)
+
+        cv2.putText(frame, "FPS: {0:.2f}".format(frame_rate_calc), (30, 50), font, 1, (255, 255, 0), 2, cv2.LINE_AA)
+
+        # All the results have been drawn on the frame, so it's time to display it.
+        cv2.imshow('Object detector', frame)
+
+        t2 = cv2.getTickCount()
+        time1 = (t2 - t1) / freq
+        frame_rate_calc = 1 / time1
+
+        break
+    camera.close()
+    image = jpg(frame)
+    return Response(image, mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
 
 @app.route("/car")
 def car():
     frame = movie_frame()
     frame = resize(frame, width=300)
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    (H, W) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(frame, size=(300, 300), ddepth=cv2.CV_8U)
-    net.setInput(blob, scalefactor=1.0 / 127.5, mean=[127.5,127.5, 127.5])
+    frame = np.copy(frame.array)
+    frame.setflags(write=1)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_expanded = np.expand_dims(frame_rgb, axis=0)
 
-    detections = net.forward()
+    # Perform the actual detection by running the model with the image as input
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: frame_expanded})
+
+    # Draw the results of the detection (aka 'visulaize the results')
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        frame,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8,
+        min_score_thresh=0.40)
+
+    # All the results have been drawn on the frame, so it's time to display it.
+    cv2.imshow('Object detector', frame)
 
     image = jpg(frame)
     return Response(image, mimetype="multipart/x-mixed-replace; boundary=frame")
