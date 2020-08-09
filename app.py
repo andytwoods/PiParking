dadb2e64e3ca41646d17e65fb5bbe48914e1eac4
andy <andytwoods@gmail.com>
2020-08-09 19:39:51 +0100

    working

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
+import time
+
+import cv2
 import imutils
 from flask import Flask
+from flask import Response
+from flask_assistant import Assistant, ask
+from imutils.video import VideoStream
 from pyngrok import ngrok
 from pyngrok.conf import PyngrokConfig
-from flask import Response
-from flask import Flask
-from flask import render_template
-import threading
-from imutils.video import VideoStream
-import time
-import cv2
 
 app = Flask(__name__)
+app.config['INTEGRATIONS'] = ['ACTIONS_ON_GOOGLE']
+assist = Assistant(app, route='/', project_id='piparking-lauj')
 
 port = 5000
 
 # Open a ngrok tunnel to the dev server
+print('setting up ngrok')
 pyngrok_config = PyngrokConfig(config_path='/home/pi/.ngrok2/ngrok.yml', region='eu')
 public_url = ngrok.connect(port, options={"subdomain": 'piparking'},
                            pyngrok_config=pyngrok_config)
@@ -25,32 +27,10 @@ app.config["BASE_URL"] = public_url
 vs = VideoStream(src=0).start()
 time.sleep(1.0)
 
-car_cascade = cv2.CascadeClassifier('cars.xml')
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-@app.route("/video_feed")
+@app.route("/image")
 def video_feed():
     frame = movie_frame()
-    image = jpg(frame)
-    return Response(image, mimetype="multipart/x-mixed-replace; boundary=frame")
-
-
-@app.route("/car")
-def car():
-    frame = movie_frame()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Detects cars of different sizes in the input image
-    cars = car_cascade.detectMultiScale(gray, 1.1, 1)
-    # To draw a rectangle in each cars
-    for (x, y, w, h) in cars:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
     image = jpg(frame)
     return Response(image, mimetype="multipart/x-mixed-replace; boundary=frame")
 
@@ -65,5 +45,19 @@ def jpg(frame):
     return b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n'
 
 
+@assist.action('parking')
+def parking():
+    resp = ask("Here's a photo of the parking situation")
+
+    resp.card(text='Parking situation',
+              title='Cars',
+              img_url='https://piparking.eu.ngrok.io/image'
+              )
+
+    return resp
+
+
+
 if __name__ == '__main__':
     app.run()
+
